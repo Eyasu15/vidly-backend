3ca45056005469a0105ba2ffaf898a91b5a850ad
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-03 23:48:42 -0500

    User delete method finished

--- src/main/java/com/vidly/services/CustomerService.java
+++ src/main/java/com/vidly/services/CustomerService.java
@@ -2,7 +2,15 @@ package com.vidly.services;
 
 import org.springframework.stereotype.Service;
 
+import com.vidly.repositories.CustomerRepository;
+
 @Service
 public class CustomerService {
+	
+	private final CustomerRepository repository;
+	
+	public CustomerService(CustomerRepository repository) {
+		this.repository = repository;
+	}
 
 }
