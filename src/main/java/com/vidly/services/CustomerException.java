1c1bd059e6484529de4b8de19acba5b7f41b2578
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-03 23:53:24 -0500

    User delete method finished

---
+++ src/main/java/com/vidly/services/CustomerException.java
@@ -0,0 +1,14 @@
+package com.vidly.services;
+
+public class CustomerException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public CustomerException(String message) {
+		super(message);
+	}
+
+}
