c6e9a8924d617da266dcd9775ebd06b2b6c627ae
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-04 23:35:37 -0500

    Add new customer

--- src/main/java/com/vidly/controllers/CustomerController.java
+++ src/main/java/com/vidly/controllers/CustomerController.java
@@ -1,10 +1,15 @@
 package com.vidly.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vidly.models.Customer;
 import com.vidly.services.CustomerService;
 
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
 
 	private final CustomerService service;
@@ -13,5 +18,8 @@ public class CustomerController {
 		this.service = service;
 	}
 	
-	
+	@GetMapping("/{id}")
+	public Customer getCustomer(@PathVariable Long id) {
+		return service.getCustomer(id);
+	}
 }
