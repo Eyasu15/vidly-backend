6ca96f54d0d246d212c00ff9992a3d84f6bf45c6
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-04 23:33:45 -0500

    Add new customer

--- src/main/java/com/vidly/controllers/CustomerController.java
+++ src/main/java/com/vidly/controllers/CustomerController.java
@@ -2,8 +2,16 @@ package com.vidly.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vidly.services.CustomerService;
+
 @RestController
 public class CustomerController {
 
+	private final CustomerService service;
+	
+	public CustomerController(CustomerService service) {
+		this.service = service;
+	}
+	
 	
 }
