cdbd672a33a1559b5fdeec7f91ad9b88ba4dfa61
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-07 15:34:01 -0500

    CustomUserDetailsService

--- src/main/java/com/vidly/config/SecurityConfig.java
+++ src/main/java/com/vidly/config/SecurityConfig.java
@@ -1,5 +1,24 @@
 package com.vidly.config;
 
-public class SecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import com.vidly.services.CustomUserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	
+	@Autowired
+	private CustomUserDetailsService userDetails;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetails);
+	}
+	
 }
