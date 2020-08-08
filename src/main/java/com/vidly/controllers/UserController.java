3919083b10d41b5e23f25a33c1af47d4ad58c0f7
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-07 23:44:11 -0500

    Added JWT dependencies

--- src/main/java/com/vidly/controllers/UserController.java
+++ src/main/java/com/vidly/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.vidly.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,17 +10,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vidly.models.AuthRequest;
 import com.vidly.models.User;
 import com.vidly.services.UserService;
+import com.vidly.util.JwtUtil;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
 	private final UserService service;
+	private final JwtUtil jwtUtil;
+	private final AuthenticationManager authManager;
 	
-	public UserController(UserService service) {
+	public UserController(UserService service, JwtUtil jwtUtil, AuthenticationManager authManager) {
 		this.service = service;
+		this.jwtUtil = jwtUtil;
+		this.authManager = authManager;
 	}
 	
 	@CrossOrigin
@@ -32,6 +39,11 @@ public class UserController {
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Object> deleteUser(@PathVariable Long id) {
 		return service.deleteUser(id);
+	}
+	
+	@PostMapping("/authenticate")
+	public String generateToken(@RequestBody AuthRequest authRequest) {
+		
 	}
 	
 }
