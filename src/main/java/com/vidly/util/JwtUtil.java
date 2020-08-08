d4669d8a97ff8ebae4ffd3174f3c29810c7ea030
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-07 23:35:02 -0500

    Added JWT dependencies

--- src/main/java/com/vidly/util/JwtUtil.java
+++ src/main/java/com/vidly/util/JwtUtil.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.cglib.core.internal.Function;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import io.jsonwebtoken.Claims;
