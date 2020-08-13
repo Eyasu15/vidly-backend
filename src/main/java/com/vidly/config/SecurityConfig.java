3f957061a13748fdcf3bc55c9125ace44076b765
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-13 13:46:17 -0500

    Movies and Genre endpoints available

--- src/main/java/com/vidly/config/SecurityConfig.java
+++ src/main/java/com/vidly/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/users/login", "/users/register", "/movies")
+        http.csrf().disable().authorizeRequests().antMatchers("/users/login", "/users/register", "/movies", "/genres")
                 .permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
