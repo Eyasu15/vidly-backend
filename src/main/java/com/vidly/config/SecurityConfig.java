db3459e1f793d5c8f5430b46dd454d0e0a0fe185
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-13 13:44:56 -0500

    Jwt filter

--- src/main/java/com/vidly/config/SecurityConfig.java
+++ src/main/java/com/vidly/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/users/login", "/users/register")
+        http.csrf().disable().authorizeRequests().antMatchers("/users/login", "/users/register", "/movies")
                 .permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
