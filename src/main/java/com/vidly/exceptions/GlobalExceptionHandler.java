e67861677a58b402041570a73c7b5f218977403b
Eyasu Weld <Eyasuweld@gmail.com>
2020-08-20 13:28:30 -0500

    Refactored Global Exception handler

--- src/main/java/com/vidly/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/vidly/exceptions/GlobalExceptionHandler.java
@@ -7,11 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
-@RestController
+@RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(Exception.class)
