9c3377795d91f01db279eab580f1b82272129355
Eyasu Weld <Eyasuweld@gmail.com>
2020-09-07 23:01:39 -0500

    Movie blob

--- src/main/java/com/vidly/models/MovieBlobs.java
+++ src/main/java/com/vidly/models/MovieBlobs.java
@@ -1,5 +1,17 @@
 package com.vidly.models;
 
+import java.sql.Blob;
+
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+
+@Entity
 public class MovieBlobs {
 
+	@Lob
+	private Blob cover;
+	
+	@OneToOne
+	private Movie movie;
 }
